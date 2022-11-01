f9b5fdf471e0aa0a0baf3844d6a4f19f8d166a14
Dominick <dag062980@gmail.com>
2022-10-31 21:07:30 -0500

    mesh add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { WalletProvider } from "../contexts/wallet";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WalletProvider>
+      <Component {...pageProps} />
+    </WalletProvider>
+  );
 }
+
+export default MyApp;
